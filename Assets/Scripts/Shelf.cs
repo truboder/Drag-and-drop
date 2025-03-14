585a3637e70a1ada0fe76b01a6ebd9d46d33431e
truboder <danil@rock-and-roll.ru>
2025-03-14 19:55:48 +0300

    Add Scripts

---
+++ Assets/Scripts/Shelf.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shelf : MonoBehaviour
+{
+    private List<DraggableObject> _draggableObjects = new List<DraggableObject>();
+
+
+}
